32c9c58358387b473ae4568c76cc3977e18d7df3
valentin viger <vigervalentin@gmail.com>
2021-09-13 14:45:57 +0200

    1st commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Lair of Wisdom - Dragon stats checker
+
+## Install 
+
+Install dependencies :  
+
+`pip3 install -r requirements.txt` (or just pip, depending on your environment)  
+
+Fill the `endpoint_url`in `dragon_checker.py`  
+
+Run : 
+
+`python3 dragon_checker.py <address_of_the_dragon>`  
+
+(Dragon addresses can be found in `dragon_address.txt`)
---
+++ dragon_checker.py
@@ -0,0 +1,43 @@
+from web3 import Web3, HTTPProvider
+import sys
+import os
+import json
+import time
+import datetime
+
+#---------------------------
+
+endpoint_url = ""
+
+#---------------------------
+
+Dragon_address = sys.argv[1]
+
+w3 = Web3(HTTPProvider(endpoint_url,request_kwargs={'timeout':60})) #Kovan node
+
+with open("./abi/Dragon_abi.json") as f:
+    Dragon_ABI = json.load(f)
+Dragon = w3.eth.contract(abi=Dragon_ABI, address=Dragon_address)
+
+dragon_name = Dragon.functions.name().call()
+
+while(True):
+    os.system('clear')
+    print('-----------------')
+    print("Dragon ", dragon_name, Dragon_address)
+    print("Health ", Dragon.functions.health().call(), "/", Dragon.functions.maxHealth().call())
+    print("Health Regen ", Dragon.functions.healthRegeneration().call())
+    print("Damages ", Dragon.functions.damage().call())
+    print('-----------------')
+    print("Uncleanliness ", Dragon.functions.getUncleanliness().call())
+    print("Sleepiness ", Dragon.functions.getSleepiness().call())
+    print("Hunger ", Dragon.functions.getHunger().call())
+    print("Boredom ", Dragon.functions.getBoredom().call())
+    print('-----------------')
+    breed_cooldown = Dragon.functions.secondsUntilBreed().call()
+    print("Can breed in  ",str(datetime.timedelta(seconds=breed_cooldown)))
+    print('-----------------')
+    atk_cooldown = Dragon.functions.secondsUntilAttack().call()
+    print("Can attack in  ",str(datetime.timedelta(seconds=atk_cooldown)))
+    print('-----------------')
+    time.sleep(30)
